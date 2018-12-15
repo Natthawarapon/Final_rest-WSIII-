4d5fccfb4312220a8adccaf7d3e64c8f8b199a35
natthawarapon <s5930213012@phuket.psu.ac.th>
2018-12-15 14:18:06 +0700

    edit

--- database.js
+++ database.js
@@ -1,12 +1,9 @@
 const pgp = require('pg-promise')();
-var db = pgp('postgres://ldhpmypdhuhmhl:3dafa54569c7d1668270ee1e368bceed03d36f2be9243c0af05b5f892d5e434c@ec2-107-21-125-209.compute-1.amazonaws.com:5432/d8ulmgf7pv5e3v?ssl=true');
+var db = pgp('postgres://mqvvplpgwkwaww:bbe283a5d4ce86d696c0794bfca51f66630f8553225b83bff3880b169880250a@ec2-54-243-147-162.compute-1.amazonaws.com:5432/ddhq6akm6qbbtp?ssl=true');
 // Add queries here
 module.exports = {
     getAllCategories
-    // getProductByID,
-    // insertProduct,
-    // updateProduct,
-    // deleteProduct
+   
 
 };
 
@@ -24,59 +21,3 @@ function getAllCategories(req, res) {
             console.log('ERROR:', error)
         })
 }
-
-// function getProductByID(req, res) {
-//     db.any('select * from products where product_id =' + req.params.id)
-//         .then(function (data) {
-//             res.status(200)
-//                 .json({
-//                     status: 'success',
-//                     data: data,
-//                     message: 'Retrieved products id:' + req.params.id
-//                 });
-//         })
-//         .catch(function (error) {
-//             console.log('ERROR:', error)
-//         })
-// }
-
-// function insertProduct( req, res) {
-//     db.none('insert into products(product_id, title, price, created_at, tags)' +
-//         'values(${product_id}, ${title}, ${price}, ${created_at}, ${tags})', req.body)
-//         .then(function (data) {
-//             res.status(200)
-//                 .json({
-//                     status: 'success',
-//                     message: 'Inserted one product'
-//                 });
-//         })
-//         .catch(function (error) {
-//             console.log('ERROR:', error)
-//         })
-// }
-// function updateProduct(req, res) {
-//     db.none('UPDATE products set product_id = ${product_id}, title = ${title}, price = ${price}, created_at = ${created_at}, tags =${tags} ' + ' where product_id =' + req.params.id,req.body)
-//         .then(function (data) {
-//             res.status(200)
-//                 .json({
-//                     status: 'success',
-//                     message: 'Updated one product'
-//                 });
-//         })
-//         .catch(function (error) {
-//             console.log('ERROR:', error)
-//         })
-// }
-// function deleteProduct(req, res) {
-//     db.none('delete from products where product_id ='+ req.params.id , req.body)
-//         .then(function (data) {
-//             res.status(200)
-//                 .json({
-//                     status: 'success',
-//                     message: 'Delete one product'
-//                 });
-//         })
-//         .catch(function (error) {
-//             console.log('ERROR:', error)
-//         })
-// }
