466e73d46a64f5e930361dbc2f965680229c1a16
natthawarapon <s5930213012@phuket.psu.ac.th>
2018-12-15 15:52:23 +0700

    edit cors

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
 var express = require('express');
 var app = express();
-
+var db = require('./database')
 var cors = require('cors');
-var db = require('./database');
-app.use(cors());
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-extended: true
-}));
-//Add routing 
+app.use(cors())
+ 
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); //รับ json เข้ามา
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Add routing
 // index page
+
 app.get('/', function (req, res) {
-res.send('Express is running');
+    res.send('Express is running');
 });
 
-var output ={
+
+var output = {
     status: 'success',
     message: 'REST API is working'
-
 }
-app.get('/api/json',function (req, res) {
-res.status(500).json(output);
+
+
+app.get('/api/json', function (req, res) {
+    res.status(500).json(output);
+
 });
 
-app.get('/api/categories/',db.getAllCategories);
+app.get('/api/categories/', db.getAllCategories);
 
-//start process
+
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
-console.log('App is running on http://localhost:' + port);
+    console.log('App is running on http://localhost:' + port);
 });
